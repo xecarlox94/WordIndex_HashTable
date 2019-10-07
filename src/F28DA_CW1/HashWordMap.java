6c6b5b606142125fa957ea5a8c50e038f3114b2c
Jose Fernandes <jcc8@hw.ac.uk>
2019-10-07 09:41:41 +0100

    Initial setup done - HashWordMap, ListWordMap and WordException files added

---
+++ src/F28DA_CW1/HashWordMap.java
@@ -0,0 +1,50 @@
+package F28DA_CW1;
+
+import java.util.Iterator;
+
+public class HashWordMap implements IWordMap {
+	
+	public HashWordMap(float f) {}
+
+	@Override
+	public void addPos(String word, IPosition pos) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeWord(String word) throws WordException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removePos(String word, IPosition pos) throws WordException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Iterator<String> words() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<IPosition> positions(String word) throws WordException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numberOfEntries() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float averNumProbes() {
+		// TODO Auto-generated method stub
+		return 0f;
+	}
+
+}
---
+++ src/F28DA_CW1/WordException.java
@@ -0,0 +1,5 @@
+package F28DA_CW1;
+
+public class WordException extends Exception {
+
+}
