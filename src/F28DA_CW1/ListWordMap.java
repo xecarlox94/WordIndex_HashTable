242a48a02dfabfbfd6f98f01b03d14cd07bba514
Jose Fernandes <jcc8@hw.ac.uk>
2019-10-08 16:45:42 +0100

    List word map functions implemented, apart from positions one

--- src/F28DA_CW1/ListWordMap.java
+++ src/F28DA_CW1/ListWordMap.java
@@ -1,43 +1,112 @@
 package F28DA_CW1;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 
-public class ListWordMap implements IWordMap {
+public class ListWordMap implements IWordMap
+{
+	// Java linked list which will store the word position data
+	private LinkedList<WordPosition> list = new LinkedList<WordPosition>();
 
 	@Override
-	public void addPos(String word, IPosition pos) {
-		// TODO Auto-generated method stub
-
+	public void addPos(String word, IPosition pos)
+	{
+		// Instantiates a new word entry
+		WordPosition wp = new WordPosition(pos.getFileName(), pos.getLine(), word);
+		
+		// tests if the linked list already contains this element
+		if (!list.contains(wp))
+		{
+			// adds a new element to the linked list
+			list.add(wp);
+		}
 	}
 
 	@Override
-	public void removeWord(String word) throws WordException {
-		// TODO Auto-generated method stub
-
+	public void removeWord(String word) throws WordException
+	{
+		// checks if any word was removed from the map
+		boolean wasAnyWordRemoved = false;
+		
+		// loops through the linked list
+		for(int i = 0; i < list.size(); i++)
+		{
+			// Instantiates a temporary string containing an index element's word for comparison
+			String tempWord = list.get(i).getWord();
+			
+			// compares the temporary string to test if it matches the given word string
+			if(tempWord.equals(word))
+			{
+				// removes the index element matching the condition
+				list.remove(i);
+				wasAnyWordRemoved = true;
+			}
+		}
+		// if no word was removed, then the word exception error is thrown stating that no entry was removed
+		if(!wasAnyWordRemoved) throw new WordException("No word position entry was removed");
 	}
 
 	@Override
-	public void removePos(String word, IPosition pos) throws WordException {
-		// TODO Auto-generated method stub
-
+	public void removePos(String word, IPosition pos) throws WordException
+	{
+		// checks if the word position entry was removed
+		boolean wasWordRemoved;
+		
+		// Instantiates a new word position with the data given
+		WordPosition wp = new WordPosition(pos.getFileName(), pos.getLine(), word);
+		
+		// removes the word position entry from the linked list and returns true
+		// if the linked list did not contain the entry it will return false
+		wasWordRemoved = list.remove(wp);
+		
+		// if no entry was removed, then throw the word exception stating that no word position entry was removed
+		if(!wasWordRemoved) throw new WordException("No word position entry was removed");
 	}
 
 	@Override
-	public Iterator<String> words() {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterator<String> words()
+	{
+		// Instantiates a temporary linked list to contain words
+		LinkedList<String> tempWordList = new LinkedList<String>();
+		
+		// loops through the linked list
+		for(int i = 0; i < list.size(); i++)
+		{
+			// stores the word string of each word position linked list 
+			String tempString = list.get(i).getWord();
+			
+			// tests if the temporary string word linked list already contains the temporary string
+			if(!tempWordList.contains(tempString))
+			{
+				// adding the strin if the temporary words list does not contain the temporary string yet
+				tempWordList.add(tempString);
+			}
+		}
+		// it returns an Iterator containing all the different words, not repeated
+		return tempWordList.iterator();
 	}
 
 	@Override
-	public Iterator<IPosition> positions(String word) throws WordException {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterator<IPosition> positions(String word) throws WordException
+	{
+		// Instantiates a temporary position linked list containing word positions
+		LinkedList<IPosition> tempPositionList = new LinkedList<IPosition>();
+		
+		// loops through the linked list
+		for(int i = 0; i < list.size(); i++)
+		{
+			
+		}
+		
+		return tempPositionList.iterator();
 	}
 
 	@Override
-	public int numberOfEntries() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int numberOfEntries()
+	{
+		// it returns word position linked list size
+		return this.list.size();
 	}
+
 
 }
--- src/F28DA_CW1/WordException.java
+++ src/F28DA_CW1/WordException.java
@@ -3,8 +3,8 @@ package F28DA_CW1;
 public class WordException extends Exception {
 	
 	//Constructor for the class takes a String containing the error message
-	public WordException(String errorMsg) {
-		
+	public WordException(String errorMsg) 
+	{
 		// it calls its super Exception, using its constructor error message
 		super(errorMsg);
 	}
