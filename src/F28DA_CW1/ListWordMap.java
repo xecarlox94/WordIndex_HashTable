18c4c855df4ec8cb8709e5f7ae32ef921bb6b4c5
Jose Fernandes <jcc8@hw.ac.uk>
2019-10-11 09:08:38 +0100

    Word Entry and ListWordMap modified

--- src/F28DA_CW1/ListWordMap.java
+++ src/F28DA_CW1/ListWordMap.java
@@ -5,18 +5,45 @@ import java.util.LinkedList;
 
 public class ListWordMap implements IWordMap
 {
-	// Java linked list which will store the word position data
+	// Java linked list which will store Word Entries
 	private LinkedList<WordEntry> list = new LinkedList<WordEntry>();
 
 	
 	@Override
 	public void addPos(String word, IPosition pos)
 	{
-		// Instantiates a new word entry
-		WordPosition wp = new WordPosition(pos.getFileName(), pos.getLine(), word);
-
-		// adds a new element to the linked list
-		list.add(wp);
+		// checks if entry was found
+		boolean entryFound = false;
+		
+		// loops through the linked list of entry to find the entry
+		for(int i = 0; i < this.list.size(); i++) 
+		{
+			// stores the word entry key in a string variable, for comparison
+			String temp = this.list.get(i).getKey();
+			
+			// check if the word entry key string is equal to the string given
+			if( temp.equals(word) )
+			{
+				// entry was found
+				entryFound = true;
+				
+				// position was added to the word entry
+				this.list.get(i).addPosition(pos);
+				
+				// finishes loop execution
+				break;
+			}
+		}
+		
+		// if entry was not found, create a new entry and add it to linked list
+		if(!entryFound)
+		{
+			// creates a temp word entry
+			WordEntry temp = new WordEntry(word,pos);
+			
+			// adds it to the linked list
+			this.list.add(temp);
+		}
 	}
 
 	@Override
@@ -29,7 +56,7 @@ public class ListWordMap implements IWordMap
 		for(int i = 0; i < list.size(); i++)
 		{
 			// Instantiates a temporary string containing an index element's word for comparison
-			String tempWord = list.get(i).getWord();
+			String tempWord = list.get(i).getKey();
 			
 			// compares the temporary string to test if it matches the given word string
 			if(tempWord.equals(word))
@@ -37,28 +64,40 @@ public class ListWordMap implements IWordMap
 				// removes the index element matching the condition
 				list.remove(i);
 				wasWordRemoved = true;
+				
+				// Finishes the loop execution
+				break;
 			}
 		}
 		
-		// if no word was removed, then the word exception error is thrown stating that no entry was removed
-		if(!wasWordRemoved) throw new WordException("No word position entry was removed");
+		// if no word was removed, then the word exception error is thrown stating that no word entry was removed
+		if(!wasWordRemoved) throw new WordException("No word entry was found");
 	}
 
 	@Override
 	public void removePos(String word, IPosition pos) throws WordException
 	{
 		// checks if the word position entry was removed
-		boolean wasWordRemoved;
+		boolean wasPosRemoved = false;
 		
-		// Instantiates a new word position with the data given
-		WordPosition wp = new WordPosition(pos.getFileName(), pos.getLine(), word);
-		
-		// removes the word position entry from the linked list and returns true
-		// if the linked list did not contain the entry it will return false
-		wasWordRemoved = list.remove(wp);
+		for (int i = 0; i < this.list.size(); i++)
+		{
+			// Instantiates a temporary string containing an index element's word for comparison
+			String tempWord = list.get(i).getKey();
+			
+			// compares the temporary string to test if it matches the given word string
+			if(tempWord.equals(word))
+			{
+				
+				wasPosRemoved = list.get(i).removePosition(pos);
+				
+				// Finishes the loop execution
+				break;
+			}
+		}
 		
 		// if no entry was removed, then throw the word exception stating that no word position entry was removed
-		if(!wasWordRemoved) throw new WordException("No word position entry was removed");
+		if(!wasPosRemoved) throw new WordException("No word position was found");
 	}
 
 	@Override
@@ -66,20 +105,17 @@ public class ListWordMap implements IWordMap
 	{
 		// Instantiates a temporary linked list to contain words
 		LinkedList<String> tempWordList = new LinkedList<String>();
-		
-		// loops through the linked list
-		for(int i = 0; i < list.size(); i++)
+
+		// for of loop to get each word entry list element
+		for(WordEntry we: this.list)
 		{
-			// stores the word string of each word position linked list 
-			String tempString = list.get(i).getWord();
+			// adds each element string to a temporary string
+			String temp = we.getKey();
 			
-			// tests if the temporary string word linked list already contains the temporary string
-			if(!tempWordList.contains(tempString))
-			{
-				// adding the string if the temporary words list does not contain the temporary string yet
-				tempWordList.add(tempString);
-			}
+			// adds each string to the temporary word string list
+			tempWordList.add(temp);
 		}
+		
 		// it returns an Iterator containing all the different words, not repeated
 		return tempWordList.iterator();
 	}
@@ -87,27 +123,24 @@ public class ListWordMap implements IWordMap
 	@Override
 	public Iterator<IPosition> positions(String word) throws WordException
 	{
-		// Instantiates a temporary position linked list containing word positions
-		LinkedList<IPosition> tempPositionList = new LinkedList<IPosition>();
+		// gets the word entry matching this key string
+		WordEntry we = this.getWordEntry(word);
 		
-		// loops through the linked list
-		for(int i = 0; i < list.size(); i++)
-		{
-			// store current list element temporary variable
-			// store its string in a variable
-			WordPosition tempWord = list.get(i);
-			String tempString = tempWord.getWord();
-			
-			// check if temporary string matches with given word string
-			if(tempString.equals(word))
-			{
-				// adding word to temporary position list
-				tempPositionList.add(tempWord);
-			}
-		}
 		
-		//returns temporary position iterator containing word positions that match the word
-		return tempPositionList.iterator();
+		// if no word entry was found then throw word exception
+		if( we == null) throw new WordException("No word entry was found");
+		
+		
+		// store the existing positions in a temporary word position iterator
+		Iterator<IPosition> tempPosIterator = we.getPositionIterator();
+		
+		
+		// if the iterator is empty, then throw a word exception
+		if( tempPosIterator == null) throw new WordException("No word positions found");
+		
+		
+		// if there positions, then return the iterator
+		return tempPosIterator;
 	}
 
 	@Override
@@ -117,5 +150,21 @@ public class ListWordMap implements IWordMap
 		return this.list.size();
 	}
 
+	private WordEntry getWordEntry(String word)
+	{
+		// loops through the linked list of entry to find the entry
+		for(int i = 0; i < this.list.size(); i++) 
+		{
+			// stores the word entry key in a string variable, for comparison
+			String temp = this.list.get(i).getKey();
+			
+			// check if the word entry key string is equal to the string given
+			if( temp.equals(word) )
+			{
+				return this.list.get(i);
+			}
+		}
+		return null;
+	}
 
 }
--- src/F28DA_CW1/WordEntry.java
+++ src/F28DA_CW1/WordEntry.java
@@ -1,15 +1,31 @@
 package F28DA_CW1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class WordEntry implements Entry<String, ArrayList<IPosition>> {
 	
 	// String containing string word
-	String word;
+	private String word;
 	
 	// List containing all positions
-	ArrayList<IPosition> positions;
+	private ArrayList<IPosition> positions;
 	
+	
+	// Constructor
+	public WordEntry(String word) {
+		this.word = word;
+	}
+	
+	// Constructor overload
+	public WordEntry(String word, IPosition pos)
+	{
+		// uses the main constructor
+		this(word);
+		
+		// add first position
+		this.positions.add(pos);
+	}
 
 	@Override
 	public String getKey()
@@ -20,6 +36,49 @@ public class WordEntry implements Entry<String, ArrayList<IPosition>> {
 	@Override
 	public ArrayList<IPosition> getValue() {
 		return this.positions;
+	}
+	
+	// adds a word position
+	public void addPosition(IPosition pos)
+	{
+		// checks if position was found
+		boolean posFound = false;
+		
+		// loops through the array list
+		for (int i = 0; i < this.positions.size(); i++)
+		{
+			// Stores the current position in the loop
+			IPosition temp = this.positions.get(i);
+			
+			// checks if any array list element is equal to pos 
+			if( temp.equals(pos) )
+			{
+				// the position was found
+				posFound = true;
+				
+				// finishes loop execution
+				break;
+			}
+		}
+		
+		// if there is no position, then add the unique position value
+		if(!posFound) this.positions.add(pos);
+	}
+	
+	// removes a position from the word entry's positions array list  
+	public boolean removePosition(IPosition pos)
+	{
+		// returns true if the position was found or false if it was not found
+		return this.positions.remove(pos);
+	}
+	
+	public Iterator<IPosition> getPositionIterator()
+	{
+		// return null if the array list is empty
+		if(this.positions.isEmpty()) return null;
+		
+		// return the iterator if the array list has elements
+		return this.positions.iterator();
 	}
 
 }
